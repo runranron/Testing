b1e02697514502bddbf670c304bb1357f2cecbdf
William Pelton <williamrpelton@gmail.com>
2018-04-23 16:09:47 -0500

    basic js complete

--- basic-javascript/tests/project-2.test.js
+++ basic-javascript/tests/project-2.test.js
@@ -4,3 +4,114 @@ const assert = require('chai').assert;
 const funcs = require('../src/project-2');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+
+describe('Project-2 Functions', () => {
+    describe(`''`, () => {
+        it('should be of type function', () => {
+        const getBiggest = funcs.getBiggest;
+        assert.typeOf(getBiggest, 'function');
+        });
+    });
+    describe(`''`, () => {
+        it('should be of type function', () => {
+        const greeting = funcs.greeting;
+        assert.typeOf(greeting, 'function');
+        });
+    });
+    describe(`''`, () => {
+        it('should be of type function', () => {
+        const isTenOrFive = funcs.isTenOrFive;
+        assert.typeOf(isTenOrFive, 'function');
+        });
+    });
+    describe(`''`, () => {
+        it('should be of type function', () => {
+        const isInRange = funcs.isInRange;
+        assert.typeOf(isInRange, 'function');
+        });
+    });
+    describe(`''`, () => {
+        it('should be of type function', () => {
+        const isInteger = funcs.isInteger;
+        assert.typeOf(isInteger, 'function');
+        });
+    });
+    describe(`''`, () => {
+        it('should be of type function', () => {
+        const fizzBuzz = funcs.fizzBuzz;
+        assert.typeOf(fizzBuzz, 'function');
+        });
+    });
+    describe(`''`, () => {
+        it('should be of type function', () => {
+        const isPrime = funcs.isPrime;
+        assert.typeOf(isPrime, 'function');
+        });
+    });
+    describe(`''`, () => {
+        it('should be of type function', () => {
+        const returnFirst = funcs.returnFirst;
+        assert.typeOf(returnFirst, 'function');
+        });
+    });
+    describe(`''`, () => {
+        it('should be of type function', () => {
+        const returnLast = funcs.returnLast;
+        assert.typeOf(returnLast, 'function');
+        });
+    });
+    describe(`''`, () => {
+        it('should be of type function', () => {
+        const getArrayLength = funcs.getArrayLength;
+        assert.typeOf(getArrayLength, 'function');
+        });
+    });
+    describe(`''`, () => {
+        it('should be of type function', () => {
+        const incrementByOne = funcs.incrementByOne;
+        assert.typeOf(incrementByOne, 'function');
+        });
+    });
+    describe(`''`, () => {
+        it('should be of type function', () => {
+        const addItemToArray = funcs.addItemToArray;
+        assert.typeOf(addItemToArray, 'function');
+        });
+    });
+    describe(`''`, () => {
+        it('should be of type function', () => {
+        const addItemToFront = funcs.addItemToFront;
+        assert.typeOf(addItemToFront, 'function');
+        });
+    });
+    describe(`''`, () => {
+        it('should be of type function', () => {
+        const wordsToSentence = funcs.wordsToSentence;
+        assert.typeOf(wordsToSentence, 'function');
+        });
+    });
+    describe(`''`, () => {
+        it('should be of type function', () => {
+        const contains = funcs.contains;
+        assert.typeOf(contains, 'function');
+        });
+    });
+    describe(`''`, () => {
+        it('should be of type function', () => {
+        const addNumbers = funcs.addNumbers;
+        assert.typeOf(addNumbers, 'function');
+        });
+    });
+    describe(`''`, () => {
+        it('should be of type function', () => {
+        const averageTestScore = funcs.averageTestScore;
+        assert.typeOf(averageTestScore, 'function');
+        });
+    });
+    describe(`''`, () => {
+        it('should be of type function', () => {
+        const largestNumber = funcs.largestNumber;
+        assert.typeOf(largestNumber, 'function');
+        });
+    });
+})
